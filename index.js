4587f8680d60546f73747b0795d174818f83619c
heinzy <heinzydj@gmail.com>
2021-10-03 20:06:32 +0200

    añadimos el innnerHTML y domcontaintloades

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
-const anuncios = [
-    {
-        nombre : "Lavadora Siemens",
-        precio : 45,
-        compra: true,
-        venta: false
-    },
-    {
-        nombre : "Television panasonic",
-        precio : 1500,
-        compra: true,
-        venta: false
-    },
-    {
-        nombre : "Lavavajillas AEG",
-        precio : 345,
-        compra: true,
-        venta: false
-    },
+
+
+window.addEventListener('DOMContentLoaded', function(){
+
+
     
-]
-
-const list = document.querySelector('.post-list')
-
-for (const anuncio of anuncios){
-    const anuncioElement = document.createElement('p')
+    const anuncios = [
+        {
+            nombre : "Lavadora Siemens",
+            precio : 45,
+            enventa: "Se compra"
+        },
+        {
+            nombre : "Television panasonic",
+            precio : 1500,
+            compra: true,
+            venta: false,
+            enventa: "En venta"
+        },
+        {
+            nombre : "Lavavajillas AEG",
+            precio : 345,
+            enventa: "En venta"
+        },
+        
+    ]
     
-    anuncioElement.innerHTML = anuncio.nombre
-    list.appendChild(anuncioElement)
-}
+    
+    
+    
+    
+    function loadAnuncios(){
+    
+        const list = document.querySelector('.post-list')   
+    
+            for (const anuncio of anuncios){
+                const anuncioElement = document.createElement('p')
+                const anuncioHTML = `<div class="post">
+                    <strong class="author">${anuncio.nombre}</strong>
+                    <p class="precio"> ${anuncio.precio}</p>
+                    <p class="precio"> ${anuncio.enventa}</p>
+                    </div>`
+                anuncioElement.innerHTML = anuncioHTML
+                list.appendChild(anuncioElement)
+            }
+            
+            
+            const loader = document.querySelector(".lds-ring")
+            
+            loader.classList.toggle('hidden')
+        
+    
+    }
+    
+    const button = document.querySelector("#button-cargar")
+    
+    button.addEventListener('click', loadAnuncios)
 
+})
 
-const loader = document.querySelector(".lds-ring")
-
-loader.classList.toggle('hidden')
 
